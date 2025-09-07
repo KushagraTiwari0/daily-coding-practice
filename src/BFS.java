242945d1381d482b168fb9c7aae3c59f61d29dfb
Kushagra Tiwari <kushagratiwari86@gmail.com>
2025-09-07 18:04:41 +0530

    Shortest distance using bfs

--- src/BFS.java
+++ src/BFS.java
@@ -7,23 +7,35 @@ public class BFS {
     {
         boolean visited[]=new boolean[v];
         LinkedList<Integer> q=new LinkedList<>();
+        int dist[]=new int[v];
+
+
+        for (int i = 0; i < v; i++) {
+            dist[i] = -1;
+        }
+        dist[src]=0;
         q.addLast(src);
         visited[src]=true;
         while(!q.isEmpty())
         {
             int node=q.removeFirst();
-            System.out.println(node);
+            //System.out.println(node);
             Iterator<Integer>it=adj[node].listIterator();
             while (it.hasNext())
             {
                 int nbr= it.next();
                 if(!visited[nbr]) {
                     visited[nbr] = true;
+                    dist[nbr]=1+dist[node];
                     q.addLast(nbr);
                 }
 
             }
-        }
+
+            }
+            for (int i=0;i<v;i++)
+                System.out.println("Node  "+i+"  Distance "+dist[i]);
+
 
     }
 
