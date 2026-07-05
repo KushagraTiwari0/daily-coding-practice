e75a10465f39505bb0aefde47e1d211dba014625
Kushagra Tiwari <kushagratiwari86@gmail.com>
2026-07-05 20:35:31 +0000

    3 Sum Completed on 5 July

---
+++ src/StriverSheet/Array/Hard/ThreeSum.java
@@ -0,0 +1,72 @@
+package StriverSheet.Array.Hard;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.List;
+
+class ThreeSum {
+
+    public static void sum(int a[],int n)
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(a);
+        for(int i=0;i<n;i++)
+        {
+            if(i>0&&a[i]==a[i-1])
+            {
+                continue;
+            }
+            int left=i+1;
+            int right=n-1;
+            while(left<right)
+            {
+                int s=a[i]+a[left]+a[right];
+                if(s==0)
+                {
+                    ans.add(Arrays.asList(a[i],a[left],a[right]));
+                    left++;
+                    right--;
+                    while (left<right&& a[left]==a[left-1]) 
+                    {
+                        left++;
+                    }
+                    while(left<right && a[right]==a[right+1])
+                    {
+                        right--;
+                    }
+                }
+                else if(s<0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+        for (List<Integer> triplet : ans) 
+        {
+            for (int num : triplet) 
+            {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the size -> ");
+        int n=sc.nextInt();
+        System.out.println();
+        System.out.print("Enter Elements ->");
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        
+        sum(a,n);
+    }
+}
